1a527a2d8ff1a9a89dfae2cd77b9c04acb492815
Sanjay Soundarajan <sanjay.16@live.com>
2022-08-30 15:42:46 -0700

    remove: 🔥 delete blog folder

--- api/index.js
+++ api/index.js
@@ -54,5 +54,5 @@ module.exports = app;
 // const port = process.env.PORT || 5000;
 
 // app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
+//   console.log(`api listening at http://localhost:${port}`);
 // });
