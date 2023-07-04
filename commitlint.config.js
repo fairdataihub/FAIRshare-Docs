986962a0bf65a1b04d3197d4bd12f4cf829d0d86
Sanjay Soundarajan <sanjay.16@live.com>
2023-07-03 17:27:02 -0700

    ci: 👷 update commit message length

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,4 +3,10 @@ const { commitlintConfig } = require('@fairdataihub/config');
 
 const config = commitlintConfig();
 
+const scopes = ['app', 'library', 'backend', 'authentication', 'settings', 'deps', 'release'];
+
+config.rules['scope-enum'] = [2, 'always', scopes];
+config.rules['body-max-line-length'] = [2, 'always', 99999999999];
+config.rules['footer-max-line-length'] = [2, 'always', 99999999999];
+
 module.exports = config;
